e9ac2fdc27ea254a1b898102b19fd6b58b35bc83
Arif Budiman <arif.budiman.10.96@gmail.com>
2022-09-21 19:20:44 +0700

    init project 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_pull_request
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
